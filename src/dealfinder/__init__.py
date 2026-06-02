99c1809c78dfbc1bf47f5482fc4f5e4cd4975e07
Ralf Schröder <ralf.schroder@suninternational.com>
2026-06-02 13:18:23 +0200

    chore: scaffold dealfinder package + tooling

---
+++ src/dealfinder/__init__.py
@@ -0,0 +1,3 @@
+"""DealFinderSA - vehicle-flipping deal radar."""
+
+__version__ = "0.1.0"
---
+++ tests/test_smoke.py
@@ -0,0 +1,5 @@
+import dealfinder
+
+
+def test_package_imports():
+    assert dealfinder.__version__ == "0.1.0"
